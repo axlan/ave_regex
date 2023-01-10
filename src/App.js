c164a7fd31f1c5c919b06d50ad96dca8e022e601
axlan <feros32@gmail.com>
2023-01-09 20:42:30 -0800

    Ugly test of basic code.

--- src/App.js
+++ src/App.js
@@ -3,95 +3,70 @@ import "./styles.css";
 import React, { useState } from "react";
 
 function App() {
-  const questions = [
+  const level_data = [
     {
-      questionText: "Who is Prime Minister of India?",
-      answerOptions: [
-        { answerText: "Vijay Rupani", isCorrect: false },
-        { answerText: "Manmohan singh", isCorrect: false },
-        { answerText: "Narendra Modi", isCorrect: true },
-        { answerText: "Deep Patel", isCorrect: false }
-      ]
-    },
-    {
-      questionText: "Who is CEO of Tata?",
-      answerOptions: [
-        { answerText: "Jeff Bezos", isCorrect: false },
-        { answerText: "Ratan Tata", isCorrect: true },
-        { answerText: "Mukesh Ambani", isCorrect: false },
-        { answerText: "Gautam Adani", isCorrect: false }
-      ]
-    },
-    {
-      questionText: "who is richest person in the world?",
-      answerOptions: [
-        { answerText: "Jeff Bezos", isCorrect: false },
-        { answerText: "Elon Musk", isCorrect: true },
-        { answerText: "Mukesh Ambani", isCorrect: false },
-        { answerText: "Warren Buffett", isCorrect: false }
-      ]
-    },
-    {
-      questionText: "how many countries in the world?",
-      answerOptions: [
-        { answerText: "120", isCorrect: false },
-        { answerText: "183", isCorrect: false },
-        { answerText: "170", isCorrect: false },
-        { answerText: "195", isCorrect: true }
-      ]
+      original_text: `Susie, forgive me Darling, for every word I say - my heart is full of you, none other than you is in my thoughts, yet
+    when I seek to say to you something not for the world, words fail me. If you were here - and Oh that you were, my
+    Susie, we need not take at all, our eyes would whisper for us, and your hand fast in mine.`,
+      prompt: `Ahg! I accidentally sent the message with my ex's name!`,
+      goal: `Replace instances of "Susie" with "Lisa".`,
+      answer: `Lisa, forgive me Darling, for every word I say - my heart is full of you, none other than you is in my thoughts, yet
+    when I seek to say to you something not for the world, words fail me. If you were here - and Oh that you were, my
+    Lisa, we need not take at all, our eyes would whisper for us, and your hand fast in mine.`
     }
   ];
 
-  const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [showScore, setShowScore] = useState(false);
-  const [score, setScore] = useState(0);
-  const handleAnswerButtonClick = (isCorrect) => {
-    if (isCorrect === true) {
-      setScore(score + 1);
-    }
+  const [userRegex, setUserRegex] = useState("");
+  const [userSubst, setUserSubst] = useState("");
+  const handleAnswerButtonClick = () => {
+    // if (isCorrect === true) {
+    //   setScore(score + 1);
+    // }
 
-    const nextQuetions = currentQuestion + 1;
-    if (nextQuetions < questions.length) {
-      setCurrentQuestion(nextQuetions);
-    } else {
-      setShowScore(true);
-    }
+    // const nextQuetions = currentQuestion + 1;
+    // if (nextQuetions < questions.length) {
+    //   setCurrentQuestion(nextQuetions);
+    // } else {
+    //   setShowScore(true);
+    // }
+    console.log(level_data[0].original_text.replace(userRegex, userSubst));
   };
 
   return (
     <>
-      <h1 className="header">Quiz</h1>
+      <h1 className="header">A bad name</h1>
       <div className="app">
-        {showScore ? (
-          <div className="score-section">
-            You scored {score} out of {questions.length}
-          </div>
-        ) : (
-          <>
-            <div className="question-section">
-              <div className="question-count">
-                <span>Question {currentQuestion + 1}</span>
-                {questions.length}
-              </div>
-              <div className="question-text">
-                {questions[currentQuestion].questionText}
-              </div>
-            </div>
+        <p>
+          {level_data[0].prompt}
+          <br />
+          <br />
+          [Goal]: {level_data[0].goal}
+        </p>
+      </div>
 
-            <div className="answer-section">
-              {questions[currentQuestion].answerOptions.map((answerOptions) => (
-                <button
+      <br />
+
+      <div className="app">
+        <form>
+          <label>
+            Regex:
+            <textarea value={userRegex} onChange={event => setUserRegex(event.target.value)} />
+          </label>
+          <label>
+            Substitution:
+            <textarea value={userSubst} onChange={event => setUserSubst(event.target.value)} />
+          </label>
+        </form>
+
+        <button
                   onClick={() =>
-                    handleAnswerButtonClick(answerOptions.isCorrect)
+                    handleAnswerButtonClick()
                   }
                 >
-                  {answerOptions.answerText}
+                  Submit
                 </button>
-              ))}
-            </div>
-          </>
-        )}
       </div>
+      {userRegex}
     </>
   );
 }
